dc7c222709eddf8ac8d3fb8bbeaa956c592b98a8
Yo_4040 <51785135+Yo4040@users.noreply.github.com>
2021-09-23 13:01:28 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 //同ディレクトリ内のindex.htmlのためのjsファイル
 console.log("ok");
 let i = 0;
-while (i <　1) {
+while (i < 1) {
     alert("あなたのPCはウィルスに感染しました。即座にNMまで¥500支払え");
     i++;
 }
+
+let message = "NMに毎月¥1,253をを支払うように。";
+
+const kure = () => {
+    if (confirm(message)) {
+        alert("この端末の電話番号から銀行口座を参照し、\n引き落としを開始します。");
+    } else {
+        kure();
+    }
+}
+kure();
